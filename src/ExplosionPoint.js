0fbec33150a34284f8f7641c78723a9f1d2b69cf
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2020-12-25 14:53:02 -0500

    style: fix spacing

--- src/ExplosionPoint.js
+++ src/ExplosionPoint.js
@@ -5,60 +5,74 @@
  * @version 2016-05-05
  */
 
-(function() { //IFFE
-    "use strict";
+(function () {
+  //IFFE
+  "use strict";
 
+  /**
+   * Constructeur de la classe ExplosionPoint
+   *
+   * @param {Phaser.Game} pJeu Référence au jeu en cours
+   * @param {Number} pPosX Position du trou sur l'axe des x
+   * @param {Number} pPosY Position du trou sur l'axe des y
+   * @param {String} pText Le texte à afficher
+   * @param {Object} pStyle Le style du texte
+   */
+  var ExplosionPoint = function ExplosionPoint(
+    pJeu,
+    pPosX,
+    pPosY,
+    pText,
+    pStyle
+  ) {
+    //Appel du constructeur parent
+    Phaser.Text.call(this, pJeu, pPosX, pPosY, pText, pStyle);
+    this.anchor.set(0.5);
 
-    /**
-     * Constructeur de la classe ExplosionPoint
-     *
-     * @param {Phaser.Game} pJeu Référence au jeu en cours
-     * @param {Number} pPosX Position du trou sur l'axe des x
-     * @param {Number} pPosY Position du trou sur l'axe des y
-     * @param {String} pText Le texte à afficher
-     * @param {Object} pStyle Le style du texte
-     */
-    var ExplosionPoint = function ExplosionPoint(pJeu, pPosX, pPosY, pText, pStyle) {
+    //Enregistrer la référence au jeu
+    this.leJeu = pJeu;
 
-        //Appel du constructeur parent
-        Phaser.Text.call(this, pJeu, pPosX, pPosY, pText, pStyle);
-        this.anchor.set(0.5);
+    //Mettre ce point dans l'affichage du jeu
+    this.leJeu.add.existing(this);
 
-        //Enregistrer la référence au jeu
-        this.leJeu = pJeu;
+    //Animer ce point
+    this.animerPoint();
+  };
 
-        //Mettre ce point dans l'affichage du jeu
-        this.leJeu.add.existing(this);
+  //Ajustements pour l'héritage
+  ExplosionPoint.prototype = Object.create(Phaser.Text.prototype);
+  ExplosionPoint.prototype.constructor = ExplosionPoint;
 
-        //Animer ce point
-        this.animerPoint();
-    };
+  ExplosionPoint.prototype.animerPoint = function () {
+    var animAlpha = this.leJeu.add.tween(this).to(
+      {
+        alpha: 0,
+      },
+      950,
+      Phaser.Easing.Linear.None,
+      true
+    );
+    //Animation de l'échelle
+    //Échelle initiale
+    this.scale.setTo(1, 1);
+    var animEchelle = this.leJeu.add.tween(this.scale);
+    animEchelle.to(
+      {
+        x: 2,
+        y: 2,
+      },
+      1000,
+      Phaser.Easing.Linear.None,
+      true
+    );
 
-    //Ajustements pour l'héritage
-    ExplosionPoint.prototype = Object.create(Phaser.Text.prototype);
-    ExplosionPoint.prototype.constructor = ExplosionPoint;
+    //Quand l'animation est terminée, on détruit le point
+    animEchelle.onComplete.add(function () {
+      //On détruit le point
+      this.destroy();
+    }, this);
+  };
 
-    ExplosionPoint.prototype.animerPoint = function() {
-        var animAlpha = this.leJeu.add.tween(this).to({
-            alpha: 0
-        }, 950, Phaser.Easing.Linear.None, true);
-        //Animation de l'échelle
-        //Échelle initiale
-        this.scale.setTo(1, 1);
-        var animEchelle = this.leJeu.add.tween(this.scale);
-        animEchelle.to({
-            x: 2,
-            y: 2
-        }, 1000, Phaser.Easing.Linear.None, true);
-
-        //Quand l'animation est terminée, on détruit le point
-        animEchelle.onComplete.add(function() {
-            //On détruit le point
-            this.destroy();
-        }, this);
-    };
-
-    //Rendre la classe publique
-    window.ExplosionPoint = ExplosionPoint; //Ou return ExplosionPoint;
-
-}());
+  //Rendre la classe publique
+  window.ExplosionPoint = ExplosionPoint; //Ou return ExplosionPoint;
+})();
